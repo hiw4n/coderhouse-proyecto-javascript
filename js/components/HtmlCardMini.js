38772a959b25d604c559d78af8e3f11bfa62e933
ivan <126351037+hiw4n@users.noreply.github.com>
2024-04-21 23:57:45 +0200

    fix delete card from cardMini

--- js/components/HtmlCardMini.js
+++ js/components/HtmlCardMini.js
@@ -15,7 +15,7 @@ export class HtmlCardMini {
           <h5 class="JSCardMini_content-info-name">${name}</h5>
       </div>-->
       <div class="JSCardMini_content-button">
-          <button onclick="personDeleted(${id})" href="#"
+          <button onclick="personDeleted(${id}, '.JSCard_content')" href="#"
               class="JSCardMini_content-button-invited btn btn-primary ">           
               <i class="fa-normal fa-regular fa-trash-can"></i>
               <i class="fa-hover fa-solid fa-trash-can"></i>
