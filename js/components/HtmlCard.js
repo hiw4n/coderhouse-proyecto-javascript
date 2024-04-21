a29bb6ed32edd766304b847485c79020499684b6
ivan <126351037+hiw4n@users.noreply.github.com>
2024-04-21 23:09:41 +0200

    adjust design card and nav

--- js/components/HtmlCard.js
+++ js/components/HtmlCard.js
@@ -17,32 +17,31 @@ export class HtmlCard {
                 <div class="JSCard_content-info-description">
                     <p>${description}</p>
                 </div>
-                <div class="JSCard_content-info-valor">
+                <!-- <div class="JSCard_content-info-valor">
                     {heart}
                     <span class="card-star">
                         <i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i>
                     </span>
-                </div>
+                </div> -->
             </div>
         </div>
         <div class="JSCard_content-button">
           <button onclick="personAdd(${id}, '.JSCard_content')" class=" btn btn-primary  ">
           Invitar
-            <i class="fa-solid fa-heart"></i>
-              <i class="fa-regular fa-heart"></i>
+            <i class="fa-normal fa-solid fa-heart"></i>
+            <i class="fa-hover fa-regular fa-heart"></i>
           </button>
         </div>
         <div class="JSCard_content-invited">
           <div class="JSCard_content-invited-image">
             <img src="./image/${avatar}" alt="${name}" >
-            <div class="JSCard_content-invited-image-circle"></div>
           </div>
           <h5 class="JSCard_content-invited-name">${name}</h5>
           <p> <i>Is invited</i></p>
           <button onclick="personDeleted(${id}, '.JSCard_content')" class=" btn btn-primary  ">
             Borrar
-            <i class="fa-solid fa-heart"></i>
-            <i class="fa-regular fa-heart"></i>
+            <i class="fa-normal fa-regular fa-trash-can"></i>
+            <i class="fa-hover fa-solid fa-trash-can"></i>
           </button>
         </div>
       </div>
